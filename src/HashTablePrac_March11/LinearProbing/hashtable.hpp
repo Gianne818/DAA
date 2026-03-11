8db133892e4e34b657a2e95bf835a4cb77576b48
Gianne <cruzgian008@gmail.com>
2026-03-12 05:00:57 +0800

    linear probe defunct my implementation

--- src/HashTablePrac_March11/LinearProbing/hashtable.hpp
+++ src/HashTablePrac_March11/LinearProbing/hashtable.hpp
@@ -4,81 +4,108 @@ using namespace std;
 
 class HashTable {
     char* table;
-    int size;
+    int N;
     int count;
+    const char DEFUNCT = ' ';
 
     // Use the ASCII code of the character
     int hash_code(char key) {
-        return (int) key;
+        return (int)key;
     }
 
     // This hash table uses a MAD compression function
     // where a = 59, b = 17, p = 509
     int compress(int code) {
-        return ((59*code+17)%509)%size;
+        return ((code*59+17)+509)%N;
     }
 
     // Using the knowledge that a hash function is composed of two portions
-    int hashFunction(char key) {
-        int code = hash_code(key);
-        return compress(code);
+    int hashfn(char key) {
+        return compress(hash_code(key));
     }
 
 public:
-    HashTable(int size){
-        this->size = size;
-        table = new char[size];
-        for(int i = 0; i<size; i++){
+    HashTable(int N){
+        this->N = N;
+        table = new char[N];
+        for(int i = 0; i<N; i++){
             table[i] = '\0';
         }
-        count = 0;
     }
 
+    // returns number of probing done, aka number of steps done from original hash code
+    // if unable to insert, return -1
     int insert(char key) {
-        int ind = hashFunction(key);
+        if(count == N || search(key)>=0) return -1; //if xylphy version of search, search(key)>0
+        int index = hashfn(key);
+
         int i = 0;
-        int probe = (ind + i++) % size;
+        int probe = (index + i) % N;
 
-        if(count==size) return -1;
-
-        while(table[probe]!='\0'){
-            if(table[probe]==key) return -1;
-            probe = (ind + i++) % size;
+        while(table[probe]!='\0' && table[probe]!=DEFUNCT){
+            i++;
+            probe = (index + i) % N;
         }
+
         table[probe] = key;
         count++;
-        return i-1;
+        return i;
     }
 
+    
+    // returns number of probing done, aka number of steps done from original hash code
+    // if not found, return the number of probing done until not found, but negative.
+    // returns i+1 in xylphy, since probe = (index + i++)%N
     int search(char key) {
-        int ind = hashFunction(key);
-        int i = 0; 
-        int probe = (ind+i)%size;
-        
-        int checked = 0;
-        while(checked<count){
+        int index = hashfn(key);
+
+        int i = 0;
+        int probe = (index + i) % N;
+
+        while(i<N && table[probe]!='\0'){
             if(table[probe]==key){
                 return i;
             }
-            else if (table[probe]!='\0'){
-                checked++;
-            }
-            probe = (ind + i++)%size;
+            i++;
+            probe = (index+i)%N;
         }
-
         return -i;
     }
 
+    // returns number of probing done, aka number of steps done from original hash code
+    // if unable to insert, return -1
     int remove(char key) {
-        return 0;
+        int i = search(key);  //i = search(key)-1 if we use the xylphy version of search
+        if(i<=0) return -1; // i<0 check if we use xylphy version
+
+        int index = hashfn(key);
+        int probe = (index+i)%N;
+
+        int next = (probe+1)%N;
+        
+        if(table[next] == '\0'){
+            table[probe] = '\0';
+            int temp = (probe-1 + N)%N;
+            int numChecked = 0;
+            while(table[temp]==DEFUNCT && numChecked < N){
+                table[temp] = '\0';
+                temp = (temp-1 + N)%N;
+                numChecked++;
+            }
+        }
+        else {
+            table[probe] = DEFUNCT;
+        }
+        count--;
+        return i;
     }
 
     void print() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < N; i++) {
             cout << i << "\t";
         }
         cout << "\n";
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < N; i++) {
             cout << table[i] << "\t";
         }
         cout << "\n";
